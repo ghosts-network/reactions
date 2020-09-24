748fdb4735f1fb373f10cc5243d3233358c84c41
Alexander <50077233+Sekir0@users.noreply.github.com>
2020-09-24 20:38:11 +0300

    Add swagger

--- GhostNetwork.Reactions.Api/Startup.cs
+++ GhostNetwork.Reactions.Api/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace GhostNetwork.Reactions.Api
 {
@@ -9,10 +11,28 @@ namespace GhostNetwork.Reactions.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = "Reactions", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseRouting();
+
+            app.UseSwagger(options =>
+            {
+                options.RouteTemplate = "swagger/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("v1/swagger.json", "Reactions");
+            });
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
