f2091daaf9ec72776ef99e5b21a91541f860c45c
Alexander <50077233+Sekir0@users.noreply.github.com>
2020-10-27 12:11:22 +0200

    Added check for empty author field (#13)
    * Added check for empty author field

--- GhostNetwork.Reactions.Api/Controllers/ReactionsController.cs
+++ GhostNetwork.Reactions.Api/Controllers/ReactionsController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -17,9 +18,9 @@ namespace GhostNetwork.Reactions.Api.Controllers
         }
 
         /// <summary>
-        /// Returns stats for one entity
+        /// Returns stats for one entity.
         /// </summary>
-        /// <response code="200">Returns stats for one entity</response>
+        /// <response code="200">Returns stats for one entity by key.</response>
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("{key}")]
@@ -29,16 +30,16 @@ namespace GhostNetwork.Reactions.Api.Controllers
         }
 
         /// <summary>
-        /// Add type of reaction to entity
+        /// Add type of reaction to entity.
         /// </summary>
-        /// <response code="201">Add type of reaction to entity</response>
+        /// <response code="201">Reaction is added.</response>
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPost("{key}/{type}")]
         public async Task<ActionResult<IDictionary<string, int>>> AddAsync(
             [FromRoute] string key,
             [FromRoute] string type,
-            [FromHeader] string author)
+            [Required, FromHeader] string author)
         {
             await reactionStorage.AddAsync(key, author, type);
 
@@ -46,13 +47,13 @@ namespace GhostNetwork.Reactions.Api.Controllers
         }
 
         /// <summary>
-        /// Remove type of reaction
+        /// Remove type of reaction.
         /// </summary>
-        /// <response code="200">Remove reaction by key and author</response>
+        /// <response code="200">Remove reaction by key and author.</response>
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpDelete("{key}")]
-        public async Task<ActionResult<IDictionary<string, int>>> DeleteAsync([FromRoute] string key, [FromHeader] string author)
+        public async Task<ActionResult<IDictionary<string, int>>> DeleteAsync([FromRoute] string key, [Required, FromHeader] string author)
         {
             await reactionStorage.DeleteAsync(key, author);
 
@@ -60,16 +61,16 @@ namespace GhostNetwork.Reactions.Api.Controllers
         }
 
         /// <summary>
-        /// Update type of reaction
+        /// Update type of reaction.
         /// </summary>
-        /// <response code="200">Update type of reaction by key and author</response>
+        /// <response code="200">Update type of reaction by key and author.</response>
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPut("{key}/{type}")]
         public async Task<ActionResult<IDictionary<string, int>>> UpdateAsync(
             [FromRoute] string key,
             [FromRoute] string type,
-            [FromHeader] string author)
+            [Required, FromHeader] string author)
         {
             await reactionStorage.UpdateAsync(key, type, author);
 
