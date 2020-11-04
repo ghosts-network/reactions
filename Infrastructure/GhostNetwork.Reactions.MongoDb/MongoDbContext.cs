aff87a42598b3bfd8b34045fde5578530d56b277
vladimirborodin <boberneprotiv@gmail.com>
2020-11-04 20:20:18 +0200

    Add mongo storage

---
+++ Infrastructure/GhostNetwork.Reactions.MongoDb/MongoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+
+namespace GhostNetwork.Reactions.MongoDb
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase database;
+
+        public MongoDbContext(IMongoDatabase database)
+        {
+            this.database = database;
+        }
+
+        public IMongoCollection<ReactionEntity> Reactions =>
+            database.GetCollection<ReactionEntity>("reactions");
+    }
+}
