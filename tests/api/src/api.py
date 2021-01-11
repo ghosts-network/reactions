4869c73d844df9cebb0aa8db58588b9b0a669e81
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2021-01-11 13:39:36 +0200

    Create api.py

---
+++ tests/api/src/api.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+class Api:
+  def get_reaction_by_key(self, key):
+    url = 'http://localhost:5000/reactions/' + key
+    headers = {'Content-Type': 'application/json' }
+
+    return requests.get(url, headers = headers)
+
+  def post_reaction(self, body):
+    url = 'http://localhost:5000/reactions/{key}/{type}'.format(
+        key = body['key'], type = body['type']
+    )
+    headers = {'Content-Type': 'application/json', 'Author': body['author'] }
+
+    return requests.post(url, headers = headers, data = json.dumps(body, indent = 4))
+    
+  def put_reaction(self, body):
+    url = 'http://localhost:5000/reactions/{key}/{type}'.format(
+        key = body['key'], type = body['type']
+    )
+    headers = {'Content-Type': 'application/json', 'Author': body['author'] }
+
+    return requests.put(url, headers = headers, data = json.dumps(body, indent = 4))
+
+  def delete_reaction_by_author(self, key, author):
+    url = 'http://localhost:5000/reactions/' + key + '/author'
+    headers = {'Content-Type': 'application/json', 'Author': author }
+
+    return requests.delete(url, headers = headers)
+
+  def delete_all_reactions(self, key):
+    url = 'http://localhost:5000/reactions/' + key
+    headers = {'Content-Type': 'application/json'}
+
+    return requests.delete(url, headers = headers)
