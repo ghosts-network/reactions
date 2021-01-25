262ed3b3e938b458e5e9f474bf199ad99c6aebea
Bohdan Kolomeiets <38009812+bod9i@users.noreply.github.com>
2021-01-25 15:41:39 +0200

    Update ReactionsQuery.cs

--- GhostNetwork.Reactions.Api/Models/ReactionsQuery.cs
+++ GhostNetwork.Reactions.Api/Models/ReactionsQuery.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections.Generic;
 
 namespace GhostNetwork.Reactions.Api.Models
 {
     public class ReactionsQuery
     {
+        [ObsoleteAttribute("This property is obsolete. Use 'Keys' property instead.", false)]
         public IEnumerable<string> PublicationIds { get; set; }
+        public IEnumerable<string> Keys { get; set; }
     }
 }
